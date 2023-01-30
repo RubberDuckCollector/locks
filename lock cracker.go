307e6427e0e17197a110d855cb558c006906d67e
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-01-30 08:55:49 +0000

    Update lock cracker.go

--- lock cracker.go
+++ lock cracker.go
@@ -21,7 +21,7 @@ func lock() {
                 fmt.Println(output)
 
                 if output == lock {
-                    fmt.Println("combination lock found")
+                    fmt.Println("combination lock found: ", output)
                     return
                 }
             }
