a685b22d4611fdcfa959cd6745ffdcd50a4a747f
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-01-30 18:44:01 +0000

    Create lock cracker.cr

---
+++ lock cracker.cr
@@ -0,0 +1,27 @@
+def lock
+
+    lock = "675"
+
+    0.upto(9) do |i|
+        0.upto(9) do |j|
+            0.upto(9) do |k|
+
+                i2 = i.to_s()
+                j2 = j.to_s()
+                k2 = k.to_s()
+
+                output = i2 + j2 + k2
+
+                puts output
+
+                if output == lock
+                    puts "lock combination found: #{output}"
+                    return
+                end
+            end
+        end
+    end
+    puts "combination not found"
+end
+
+lock
