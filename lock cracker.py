fd691ae73cd29f0eb99b78b00ecf3292c0de30ab
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-01-30 08:56:17 +0000

    Create lock cracker.py

---
+++ lock cracker.py
@@ -0,0 +1,25 @@
+def lock():
+
+    lock = "225"
+    
+    for i in range(0, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+
+                i2 = str(i)
+                j2 = str(j)
+                k2 = str(k)
+
+                output = i2 + j2 + k2
+
+                print(output)
+
+                if output == lock:
+                    print(f"lock combination found: {output}")
+                    return
+
+def main():
+    lock()
+
+if __name__ == '__main__':
+    main()
