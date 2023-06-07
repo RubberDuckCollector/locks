a3e50700e5fe53245e1fa1608361545fb4182eba
RubberDuckCollector <119439430+RubberDuckCollector@users.noreply.github.com>
2023-06-07 21:05:23 +0100

    Update computing the combinations of a combination lock.cpp

--- computing the combinations of a combination lock.cpp
+++ computing the combinations of a combination lock.cpp
@@ -1,71 +1,75 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 void three_digit_lock() {
-    std::vector<std::vector<int> > nums;
+    std::string combination;
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
-                nums.push_back({i, j, k});
+                std::string i2 = std::to_string(i);
+                std::string j2 = std::to_string(j);
+                std::string k2 = std::to_string(k);
+
+                combination = i2 + j2 + k2;
+
+                std::cout << combination << "\n";
             }
         }
     }
 
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums[i].size(); j++) {
-          std::cout << nums[i].at(j) << " ";
-        }
-        std::cout << "\n";
-    }
 }
 
 void four_digit_lock() {
-    std::vector<std::vector<int> > nums;
+    std::string combination;
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
                 for (int l = 0; l < 10; l++) {
-                    nums.push_back({i, j, k, l});
+                    std::string i2 = std::to_string(i);
+                    std::string j2 = std::to_string(j);
+                    std::string k2 = std::to_string(k);
+                    std::string l2 = std::to_string(l);
+
+                    combination = i2 + j2 + k2 + l2;
+
+                    std::cout << combination << "\n";
                 }
             }    
         }
     }
 
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums[i].size(); j++) {
-          std::cout << nums[i].at(j) << " ";
-        }
-        std::cout << "\n";
-    }
 }
 
 void five_digit_lock() {
-    std::vector<std::vector<int> > nums;
+    std::string combination;
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             for (int k = 0; k < 10; k++) {
                 for (int l = 0; l < 10; l++) {
                     for (int m = 0; m < 10; m++) {
-                        nums.push_back({i, j, k, l, m});
+                        std::string i2 = std::to_string(i);
+                        std::string j2 = std::to_string(j);
+                        std::string k2 = std::to_string(k);
+                        std::string l2 = std::to_string(l);
+                        std::string m2 = std::to_string(m);
+
+                        combination = i2 + j2 + k2 + l2 + m2;
+
+                        std::cout << combination << "\n";
                     }
                 }
             }    
         }
     }
 
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums[i].size(); j++) {
-          std::cout << nums[i].at(j) << " ";
-        }
-        std::cout << "\n";
-    }
 }
 
 void six_digit_lock() {
-    std::vector<std::vector<int> > nums;
+    std::string combination;
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
@@ -73,28 +77,35 @@ void six_digit_lock() {
                 for (int l = 0; l < 10; l++) {
                     for (int m = 0; m < 10; m++) {
                         for (int n = 0; n < 10; n ++) {
-                            nums.push_back({i, j, k, l, m, n});
+                            std::string i2 = std::to_string(i);
+                            std::string j2 = std::to_string(j);
+                            std::string k2 = std::to_string(k);
+                            std::string l2 = std::to_string(l);
+                            std::string m2 = std::to_string(m);
+                            std::string n2 = std::to_string(n);
+
+                            combination = i2 + j2 + k2 + l2 + m2 + n2;
+
+                            std::cout << combination << "\n";
                         }
                     }
                 }
             }    
         }
     }
-
-    for (int i = 0; i < nums.size(); i++) {
-        for (int j = 0; j < nums[i].size(); j++) {
-          std::cout << nums[i].at(j) << " ";
-        }
-        std::cout << "\n";
-    }
 }
 
 int main(void) {
     
     three_digit_lock();
+    std::cout << "\n";
+
     four_digit_lock();
+    std::cout << "\n";
+
     five_digit_lock();
+    std::cout << "\n";
+    
     six_digit_lock();
     
-    return 0;
 }
